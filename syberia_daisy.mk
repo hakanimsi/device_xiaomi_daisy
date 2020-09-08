1bfa2df24044d1d09db7f19931bc6e8969f2ac5a
lupesoltec <dev@lupesoltec.com.br>
2020-09-08 20:05:50 +0000

    Some improvements (clang, muiucam and extra apps)

--- syberia_daisy.mk
+++ syberia_daisy.mk
@@ -18,11 +18,17 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
+# Allow vendor/extra to override any property by setting it first
+$(call inherit-product-if-exists, vendor/extra/product.mk)
+
+# Inherit from MiuiCamera
+$(call inherit-product, vendor/MiuiCamera/config.mk)
+
 # Inherit from daisy device
 $(call inherit-product, device/xiaomi/daisy/device.mk)
 
 # Inherit some common Syberia stuff
-$(call inherit-product, vendor/syberia/config/common.mk)
+$(call inherit-product, vendor/syberia/common.mk)
 
 # Bootanimation
 TARGET_BOOT_ANIMATION_RES := 1080
